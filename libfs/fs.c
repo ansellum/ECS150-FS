b9ff74c56b09bdca69e3994e6a14bd7fcefa37ed
cabueg <cabueg@COE-CS-pc16.OU.AD3.UCDAVIS.EDU>
2022-11-09 01:11:58 -0800

    wrote out some structs

--- libfs/fs.c
+++ libfs/fs.c
@@ -9,6 +9,64 @@
 
 /* TODO: Phase 1 */
 
+/* 
+    The superblock is the first block of the file system. 
+    Its internal format is:
+
+    Offset	Length (bytes)	Description
+    0x00	8	            Signature (must be equal to “ECS150FS”)
+    0x08	2	            Total amount of blocks of virtual disk
+    0x0A	2	            Root directory block index
+    0x0C	2	            Data block start index
+    0x0E	2	            Amount of data blocks
+    0x10	1	            Number of blocks for FAT
+    0x11	4079	        Unused/Padding
+*/
+
+struct superblock {
+    char sig[8];                // signature: ECS150FS 
+    uint16_t totalNumOfBlocks;  // Total amount of blocks of virtual disk
+    uint16_t indexOfRootDir;    // Root directory block index
+    uint16_t indexOfDataBlocks; // Data block start index
+    uint8_t  numOfFatBlocks;    // Number of blocks for FAT
+    uint8_t  unused[4079];
+} sb;
+
+/*
+    The FAT is a flat array, possibly spanning several blocks, 
+    which entries are composed of 16-bit unsigned words. 
+    There are as many entries as data blocks in the disk.
+    Each entry in the FAT is 16-bit wide.
+
+    FAT index:	0	    1	2	3	4	5	6	    7	    8	9   10	…
+    Content:	0xFFFF	8	3	4	5	6	0xFFFF	0	0xFFFF	0	0	…
+*/
+struct fat {
+    uint16_t words;
+}__attribute__((__packed__));
+
+
+/*
+    The root directory is an array of 128 entries stored in the block following the FAT. 
+    Each entry is 32-byte wide and describes a file, according to the following format:
+
+    Offset	Length (bytes)	Description
+    0x00	16	            Filename (including NULL character)
+    0x10	4	            Size of the file (in bytes)
+    0x14	2	            Index of the first data block
+    0x16	10	            Unused/Padding
+*/
+struct rootDir {
+	// DAG (direct acyclic graph)
+    char fileName[FS_FILENAME_LEN];
+
+    uint32_t fileSize;         // Length 4 bytes file size
+    uint8_t  indexOfDataBlock; // Index of the first data block
+    uint8_t  unused[10];
+
+
+} __attribute__((__packed__));
+
 int fs_mount(const char *diskname)
 {
 	/* TODO: Phase 1 */
