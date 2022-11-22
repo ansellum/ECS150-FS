6c9ad91ca4f3e4e5cd685ce41c103c62e05057bc
ansellum <jbvogt@ucdavis.edu>
2022-11-21 16:34:25 -0800

    Added count check for writing

--- apps/testing.sh
+++ apps/testing.sh
@@ -5,16 +5,16 @@
 disk=$2
 
 if [ "$#" -lt 2 ]; then
-    printf "Usage: <command> <diskname> <...>\n"
-    printf "Possible commands are:\n"
-    printf "\tinfo\n"
-    printf "\tls\n"
-    printf "\tadd\n"
-    printf "\trm\n"
-    printf "\tcat\n"
-    printf "\tstat\n"
-    printf "\tscript\n"
-    exit 1
+	printf "Usage: <command> <diskname> <...>\n"
+	printf "Possible commands are:\n"
+	printf "\tinfo\n"
+	printf "\tls\n"
+	printf "\tadd\n"
+	printf "\trm\n"
+	printf "\tcat\n"
+	printf "\tstat\n"
+	printf "\tscript\n"
+	exit 1
 fi
 
 cp "$disk" "${disk}_REF"
@@ -28,15 +28,15 @@ printf "\n* current:\n"
 
 ## FOR VIEWING CMP ##
 if cmp -b "${disk}_REF" "${disk}_CUR" | grep 'differ'; then
-    printf "\n** DIFF REF CUR **\n"
+	printf "\n** DIFF REF CUR **\n"
 	diff -y <(od -x ${disk}_REF) <(od -x ${disk}_CUR)
 #else
-    #printf "\n** FS_REF DISK **\n"
-    #od -x "${disk}_REF"
+	#printf "\n** FS_REF DISK **\n"
+	#od -x "${disk}_REF"
 
-    #printf "\n** TEST_FS DISK **\n"
-    #od -x "${disk}_CUR"
-    
+	#printf "\n** TEST_FS DISK **\n"
+	#od -x "${disk}_CUR"
+	
 fi
 
 rm "${disk}_REF"
